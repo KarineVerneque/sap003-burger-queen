566d726dda980d5aa3054008457fa3a07d4fd7d8
KarineVerneque <karinemvs01@hotmail.com>
2019-12-13 16:58:53 -0300

    counter

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,35 +1,68 @@
 import React, { useState, useRef, useEffect } from 'react';
-import firebaseInitialize from '../../firebase/firebase'
+// import firebaseInitialize from '../../firebase/firebase'
+// import db from '../../firebase/firebase'
 
 
 
-function PrintName() {
-
-  firebaseInitialize.firestore().collection('frutas').add({
-    cor: 'amarelo',
-    peso: 'médio',
-    forma: 'redonda'
-  })
+// firebaseInitialize.firestore().collection('breakfast').get()
+//   .then((snap => snap.forEach((doc) => console.log(doc.data().name))))
 
 
-  const [name, setName] = useState('') //state hook
-  const inputRef = useRef(null) //ref hook
-  
-  const [age, setAge] = useState(42)
-  console.log(setAge)
 
-  useEffect(() => {
-    inputRef.current.value = ''
-  })
 
-return(
-  <>
-  <input type='text' ref={inputRef} placeholder='Nome'/>
-  <button onClick={() => setName(inputRef.current.value)}>Printa</button>
-  <p>{name}</p>
-  <p>Sua idade: {age}</p>
-  </>
-)
+//   firebaseInitialize.firestore().collection('lunchdinner').get()
+//   .then((snap => snap.forEach((doc) => console.log('dinner ', doc.data()))))
+
+
+function App() {
+  const [counter, setCounter] = useState(0)
+
+  // useEffect(() => {
+  // firebaseInitialize.firestore().collection('lunchdinner').get()
+  // .then((snap => snap.forEach((doc) => console.log('dinner ', doc.data()))))
+  // }, [])
+
+  return (
+    <>
+    <p>{counter}</p>
+    <button onClick={() => setCounter(counter + 1)}>Contador</button>
+    </>
+  )
 }
 
-export default PrintName;
+
+export default App;
+
+
+
+// function PrintName() {  
+
+// //  firebaseInitialize.firestore().collection('frutas').add({
+// //    cor: 'amarelo',
+// //    peso: 'médio',
+// //    forma: 'redonda'
+// //  })
+
+
+
+//   const [name, setName] = useState('') //state hook
+//   const inputRef = useRef(null) //ref hook
+  
+//   const [age, setAge] = useState(42)
+//   console.log(setAge)
+
+//   useEffect(() => {
+//     inputRef.current.value = ''
+//   })
+
+// return(
+//   <>
+//   <input type='text' ref={inputRef} placeholder='Nome'/>
+//   <button onClick={() => setName(inputRef.current.value)}>Printa</button>
+//   <p>{name}</p>
+//   <p>Sua idade: {age}</p>
+//   </>
+// )
+// }
+
+// export default PrintName;
